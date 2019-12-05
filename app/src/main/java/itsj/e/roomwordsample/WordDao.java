dac752b74ee2111a5d23c7fa726afd94d0efcf70
Benjamin Viramontes <benja120599@gmail.com>
2019-12-05 13:09:34 -0600

    Se creó la clase WordDao

---
+++ app/src/main/java/itsj/e/roomwordsample/WordDao.java
@@ -0,0 +1,22 @@
+package itsj.e.roomwordsample;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+}
